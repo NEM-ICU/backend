4411e63c7ac4221492a6bf70ac2426d6ec5eb7ec
NEM-ICU <itt1819025@tec.rjt.ac.lk>
2021-08-05 23:48:17 +0530

    just working first simple route

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 const express = require("express");
 const app = express();
 
+// get request for controller
+
+data = { 'apple': 'apple'};
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            data
+        }
+    })
+});
+
 
 
 module.exports = app;
+  
